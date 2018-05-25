da7cabd07cb210c40a6465ef7566b029b8da6fa9
Albert Kim <lisa@Lisas-MacBook-Pro.local>
2018-05-24 21:09:25 -0700

    finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -47,9 +47,9 @@ var questionList = [
     {
         questions : 'The world’s fastest growing plant is a species of what?',
         answers : {
-            a:'Bamboo',
-            b: 'Dandelion',
-            c:'Grass'
+            a: 'Bamboo',
+            b: 'Algae',
+            c: 'Weed'
         },
         correctAnswer : 'a'
     },
@@ -90,72 +90,61 @@ var questionList = [
         correctAnswer : 'b' 
     }
 ];
-console.log(questionList[0].correctAnswer)
 
 var intervalId;
 var correct = 0; //keep track of questions answered correctly;
 var incorrect = 0;
+var unanswered = 0;
 var currentQuestion = 0; //track index of array Q's & A's
 var timer = 10;
 // var ranOutOfTime = false;
-var answeredCorrectly = false;
-
+// var answeredCorrectly = false;
 
 $(document).ready(function() {
-    $('.start-button').on('click', function () {
+    $('#start-button').on('click', function () {
+        $('#start-button').remove();
         displayQuestion(); // displays first question when clicked
         displayAnswerOptions(); //displays first set of answer options
-        $('.start-button').remove();//removes start button after being clicked
+        //removes start button after being clicked
         displayTimer();
         run(); //runs timer as soon as start is clicked
     });
-
+    
     $('.button').on('click', function () {
         var buttonClicked = $(this).val(); 
-            if (currentQuestion >= questionList.length) {
-                $('#countdown-timer').html();
-                $('#quiz').empty();
-                //add restart button
-            } else if (buttonClicked === questionList[currentQuestion].correctAnswer) {
-                correct++
-                currentQuestion++
-                nextQuestionAnswer();
-                alert ('You got it right!');
-            } else if (buttonClicked !== questionList[currentQuestion].correctAnswer){
-                incorrect++ 
-                alert('Correct answer was ' + questionList[currentQuestion].correctAnswer)
-                currentQuestion++
-                nextQuestionAnswer();
-                //add displayCorrectAnswer() with timer to stop and go to nextQuestionAnswer();
-            }
+        
+        if (buttonClicked === questionList[currentQuestion].correctAnswer) {
+            stop();
+            $('#countdown-timer').empty();
+            displayCongratulations();
+        } else if (buttonClicked !== questionList[currentQuestion].correctAnswer){
+            stop();
+            $('#countdown-timer').empty();            
+            displayCorrectAnswer();
+        }
     });
 
     function displayQuestion() {
         $('#question').html('<h1>' +questionList[currentQuestion].questions + '</h1>');
-        // console.log(questionList[currentQuestion].questions)
     };
 
-    function displayCongratulations() {
-        timer = 5;
-        timer --;
+    function displayCongratulations() { // run on correct answer selection
+        $('#question').html('<h1> Noice, you got it right! </h1>')
         correct++;
-        currentQuestion++
-        $('#question').html('<h1> Congratulations, you got it right! </h1>')
-        if (timer = 0){
-            stop();
-            nextQuestionAnswer();
-        }
+        setTimeout(interim, 5000);
     };
 
-    function displayCorrectAnswer() {
-        //need timer for incorrectanswer message
-        timer = 5;
-        timer --;
-        $('#question').html('The correct answer was ' + questionList[currentQuestion].correctAnswer.toUpperCase() + '.')
-        if (timer = 0){
-            stop();
-            nextQuestionAnswer();
-        }
+    function displayCorrectAnswer() { //run on incorrect answer selection
+        $('#question').html('NOPE.The correct answer was ' + questionList[currentQuestion].correctAnswer.toUpperCase() + '.');
+        incorrect++;
+        setTimeout(interim, 5000);
+        
+    };
+
+    function ranOutOfTime() { //run on time out
+        $('#question').html('You ran out of time! The correct answer was ' + questionList[currentQuestion].correctAnswer.toUpperCase() + '.');
+        unanswered++;
+        setTimeout(interim, 5000);
     };
 
     function displayAnswerOptions() {
@@ -167,22 +156,21 @@ $(document).ready(function() {
     function nextQuestionAnswer() {
 
         if (currentQuestion >= questionList.length){
-            $('#question').html('You answered ' + correct + ' questions correctly  and '  + incorrect + ' incorrectly.');
+            $('#question').html('You answered ' + correct + ' questions correctly  and '  + incorrect + ' incorrectly. You left ' + unanswered + ' questions unanswered.');
             $('#quiz').empty();
             $('#countdown-timer').empty();
             stop(); 
             //add restart button 
-        } else if (currentQuestion <= questionList.length) {
+        } else if (currentQuestion <= questionList.length && timer > 0) {
             timer = 10;
+            stop();
+            run();  
             $('#countdown-timer').html(timer);
             $('#question').html('<h1>' +questionList[currentQuestion].questions + '</h1>' );
             $('#option1').html('<h3>A.' + questionList[currentQuestion].answers.a + '</h3>');
             $('#option2').html('<h3>B.' + questionList[currentQuestion].answers.b + '</h3>');
             $('#option3').html('<h3>C.' + questionList[currentQuestion].answers.c + '</h3>');
-            
         }
-        stop();
-        run();
     };        
     
     function displayTimer() {
@@ -190,41 +178,30 @@ $(document).ready(function() {
     }    
 
     function run() { 
-        if (currentQuestion >= questionList.length) {
-            $('#countdown-timer').empty();
-        } else {
-            displayTimer();
-            timer = 10;
-            intervalId = setInterval(countdown, 1000);
+        intervalId = setInterval(tenSecondTimer, 1000);
+        function tenSecondTimer() {
+            if (timer === 0) {
+                clearInterval(intervalId);
+                stop();
+                $('#countdown-timer').empty();
+                ranOutOfTime();
+            } else if (timer > 0) {
+                timer--;
+                $('#countdown-timer').html(timer);  
+            }
         }
-    };    
-
-    function countdown() {
-        timer--;
-        console.log(timer)
-        $('#countdown-timer').html(timer);
-        if (timer <= 0) { 
-            incorrect++;
-            stop();
-            currentQuestion++;
-            nextQuestionAnswer(); 
-        }      
-    };
+    };  
 
     function stop() { //stop function for when timer === 0
         clearInterval(intervalId)
     };
 
-    function clearTimer() {
-        clearTimeout(intervalId)
-    }
-    function restart() {
-        currentQuestion = 0;
-        correct = 0;
-        incorrect = 0;
-        timer = 10;
-        displayQuestion();
-        displayAnswerOptions();
+    function interim() {
+        if (currentQuestion < questionList.length) {
+            currentQuestion++;
+            timer = 5;
+            nextQuestionAnswer();  
+        } 
     };
 });
 
